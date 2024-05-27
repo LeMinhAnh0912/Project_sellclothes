617f6f2f9ff079677011cc2e549865ca6050ea31
LeMinhAnh0912 <162423003+LeMinhAnh0912@users.noreply.github.com>
2024-05-27 14:18:50 +0700

    build interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
