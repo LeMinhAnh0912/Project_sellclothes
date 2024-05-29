1d33c78b07714a6a82934ea6ff812d05ab741238
ThanhQuang1810 <nguyentranthanhquang1810@gmail.com>
2024-05-29 17:52:59 +0700

    feature/shopping-cart

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,26 +1,25 @@
+// src/components/detail/Detail.jsx
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/slices/cartReducer";
 import SupportAgentIcon from "@mui/icons-material/SupportAgent";
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import "./Detail.css";
+
 export default function Detail() {
   const { id } = useParams();
   const [item, setItem] = useState(null);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    console.log(`id: ${id}`);
-
     axios
       .get(`http://localhost:3000/items/${id}`)
       .then((response) => {
-        console.log("Data fetched:", response.data);
-        if (response.data) {
-          setItem(response.data);
-        } else {
-          console.error("No item found with this ID");
-        }
+        setItem(response.data);
       })
       .catch((error) => {
         console.error("Có lỗi xảy ra khi lấy dữ liệu!", error);
@@ -31,6 +30,12 @@ export default function Detail() {
     return <p>Loading...</p>;
   }
 
+  const handleAddToCart = () => {
+    dispatch(addToCart(item));
+    alert("Thêm vào giỏ hàng thành công");
+    navigate("/");
+  };
+
   return (
     <div className="product-card">
       <img src={item.image} alt={item.name} />
@@ -38,12 +43,9 @@ export default function Detail() {
         <h1>{item.name}</h1>
         <p>Price: {item.price}</p>
         <p>Description: {item.description}</p>
-        <div className="quantity-controls">
-          <button className="decrease">-</button>
-          <span>1</span>
-          <button className="increase">+</button>
-        </div>
-        <button className="add-to-cart-btn">Add to cart</button>
+        <button onClick={handleAddToCart} className="add-to-cart-btn">
+          Add to cart
+        </button>
         <p className="quality-assurance">
           <CheckCircleIcon style={{ color: "green" }} />
           Sản phẩm chất lượng - uy tín
